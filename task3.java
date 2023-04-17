0b31b1ab4ef8543f069ec187a01840344d0fde76
Catherine <kankatya@bk.ru>
2023-04-17 18:30:17 +0500

    added input system

---
+++ task3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class task3 {
+    public static void main(String[]args){
+        int stop = 1;
+        while(stop!=0){
+            Scanner input = new Scanner(System.in);
+            System.out.print("Введите первое число: ");
+            int num1 = input.nextInt();
+            System.out.print("Введите второе число: ");
+            int num2 = input.nextInt();
+
+    }
+
+}
+}
