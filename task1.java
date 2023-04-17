ad0edaa87a0f14a481036d093ad3a20c76b5c22b
Catherine <kankatya@bk.ru>
2023-04-17 18:00:59 +0500

    Realised code for algorythm

--- task1.java
+++ task1.java
@@ -8,4 +8,8 @@ public class task1 {
         int pr = 1;
         int sum = 0;
         int[]arr = new int[n+1];
+        for(int i=1; i<arr.length; ++i){
+            arr[i]=i;   pr = pr*arr[i];   sum = sum +arr[i];
+        }
+        System.out.print("Product = " + pr + "\nTriangular number = " + sum);
 }
