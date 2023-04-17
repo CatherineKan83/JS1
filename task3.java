54b6bd80f61437e22f493f19b0c402a2e9ad119b
Catherine <kankatya@bk.ru>
2023-04-17 18:37:30 +0500

    realised primitive calculator

--- task3.java
+++ task3.java
@@ -9,6 +9,29 @@ public class task3 {
             int num1 = input.nextInt();
             System.out.print("Введите второе число: ");
             int num2 = input.nextInt();
+            int a=num1+num2;
+            int b=num1-num2;
+            double c=(double)num1/(double)num2;
+            int d= num1*num2;
+            System.out.print("Какую математическую операцию хотите осуществить?\n1)+\n2)-\n3)/\n4)*\n");
+            int operation = input.nextInt();
+            if(operation==1){
+                System.out.println(a);
+            }
+            if(operation==2){
+                System.out.println(b);
+            }
+            if(operation==3){
+                System.out.println(c);
+            }
+            if(operation==4){
+                System.out.println(d);
+            }
+        System.out.println("Для завершения операций введите '0'. Чтобы продолжить нажмите '1'.");
+        stop = input.nextInt();
+        if(stop ==0){
+            input.close(); 
+        }
 
     }
 
